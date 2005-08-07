65f065a7e048fedfcaf4499260b48de259a47018
Ulrich Drepper <drepper@redhat.com>
2005-08-07 08:12:33 +0000

    merge of 647703d119f6504ae6e88716cebb7ac031005518      and b4004520e8e502f5e04c70ec25adf09fb9331acb

---
+++ tests/run-elflint-self.sh
@@ -0,0 +1,47 @@
+#! /bin/sh
+# Copyright (C) 2005 Red Hat, Inc.
+# Written by Ulrich Drepper <drepper@redhat.com>, 2005.
+#
+# This program is Open Source software; you can redistribute it and/or
+# modify it under the terms of the Open Software License version 1.0 as
+# published by the Open Source Initiative.
+#
+# You should have received a copy of the Open Software License along
+# with this program; if not, you may obtain a copy of the Open Software
+# License version 1.0 from http://www.opensource.org/licenses/osl.php or
+# by writing the Open Source Initiative c/o Lawrence Rosen, Esq.,
+# 3001 King Ranch Road, Ukiah, CA 95482.
+set -e
+
+export LD_LIBRARY_PATH=../libebl:../libelf${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH
+
+runtest() {
+# Uncomment for debuging
+#  echo $1
+  ../src/elflint --quiet --gnu-ld $1
+}
+
+runtest ../src/addr2line
+runtest ../src/elfcmp
+runtest ../src/elflint
+runtest ../src/findtextrel
+runtest ../src/ld
+runtest ../src/nm
+runtest ../src/objdump
+runtest ../src/readelf
+runtest ../src/size
+runtest ../src/strip
+runtest ../libelf/libelf.so
+runtest ../libdw/libdw.so
+runtest ../libasm/libasm.so
+runtest ../libebl/libebl_alpha.so
+runtest ../libebl/libebl_arm.so
+runtest ../libebl/libebl_i386.so
+runtest ../libebl/libebl_ia64.so
+runtest ../libebl/libebl_ppc.so
+runtest ../libebl/libebl_ppc64.so
+runtest ../libebl/libebl_sh.so
+runtest ../libebl/libebl_sparc.so
+runtest ../libebl/libebl_x86_64.so
+
+

66c650e9c03734616425b467e83c8e93a4eeeb48
Ulrich Drepper <drepper@redhat.com>
2007-01-30 16:48:56 +0000

    Update copyright year.

--- libelf/nlist.c
+++ libelf/nlist.c
@@ -1,5 +1,5 @@
 /* Extract symbol list from binary.
-   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2005 Red Hat, Inc.
+   Copyright (C) 1998, 1999, 2000, 2001, 2002, 2005, 2007 Red Hat, Inc.
    This file is part of Red Hat elfutils.
    Written by Ulrich Drepper <drepper@redhat.com>, 1998.
 

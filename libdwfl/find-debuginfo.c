cedd299acd4b42f88a0a5bf800f3d87f8225fce7
Roland McGrath <roland@redhat.com>
2010-06-30 01:06:45 -0700

    Indentation fix.

--- libdwfl/find-debuginfo.c
+++ libdwfl/find-debuginfo.c
@@ -118,8 +118,8 @@ validate (Dwfl_Module *mod, int fd, bool check, GElf_Word debuglink_crc)
 	__libdwfl_seterrno (error);
       else if (likely (__libdwfl_find_build_id (mod, false,
 						mod->debug.elf) == 2))
-	  /* Also backdoor the gratuitous flag.  */
-	  mod->debug.valid = true;
+	/* Also backdoor the gratuitous flag.  */
+	mod->debug.valid = true;
       else
 	{
 	  /* A mismatch!  */

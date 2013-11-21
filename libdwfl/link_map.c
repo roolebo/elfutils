99a29497c673871040fed2382938eb5bd06befdd
Jan Kratochvil <jan.kratochvil@redhat.com>
2013-11-21 15:33:22 +0100

    Fix non-build-id core files on build-id system
    Signed-off-by: Jan Kratochvil <jan.kratochvil@redhat.com>

--- libdwfl/link_map.c
+++ libdwfl/link_map.c
@@ -415,9 +415,11 @@ report_r_debug (uint_fast8_t elfclass, uint_fast8_t elfdata,
 						&buffer, &buffer_available,
 						build_id_vaddr, build_id_len,
 						memory_callback_arg))
-			/* File has valid build-id which cannot be verified
-			   in memory.  */
-			valid = false;
+			{
+			  /* File has valid build-id which cannot be read from
+			     memory.  This happens for core files without bit 4
+			     (0x10) set in Linux /proc/PID/coredump_filter.  */
+			}
 		      else
 			{
 			  if (memcmp (build_id_bits, buffer, build_id_len) != 0)

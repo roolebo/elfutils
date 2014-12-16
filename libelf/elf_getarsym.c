4bb122a87608a1e0f7c27341fe1b3cd05c1462be
Mark Wielaard <mjw@redhat.com>
2014-12-16 19:43:21 +0100

    libelf: Check index_size doesn't overflow in elf_getarsym.
    Signed-off-by: Mark Wielaard <mjw@redhat.com>

--- libelf/elf_getarsym.c
+++ libelf/elf_getarsym.c
@@ -182,7 +182,8 @@ elf_getarsym (elf, ptr)
       tmpbuf[10] = '\0';
       size_t index_size = atol (tmpbuf);
 
-      if (SARMAG + sizeof (struct ar_hdr) + index_size > elf->maximum_size
+      if (index_size > elf->maximum_size
+	  || elf->maximum_size - index_size < SARMAG + sizeof (struct ar_hdr)
 #if SIZE_MAX <= 4294967295U
 	  || n >= SIZE_MAX / sizeof (Elf_Arsym)
 #endif

ddbf23a4594b5a6f1b387b15d42312a203075d80
Mark Wielaard <mjw@redhat.com>
2014-12-24 13:17:23 +0100

    libdw: Search for the last matching address with dwarf_getsrc_die.
    In commit 7d9b5a dwfl_module_getsrc was changed so that it returns the last
    line record <= addr, rather than returning immediately on a match. This
    changes dwarf_getsrc_die to do the same. And it adds a new test that checks
    this by comparing against the same results from eu-addr2line (which uses
    dwfl_module_getsrc) using dwarf_addrdie and dwarf_getsrc_die instead.
    
    Signed-off-by: Mark Wielaard <mjw@redhat.com>

--- libdw/dwarf_getsrc_die.c
+++ libdw/dwarf_getsrc_die.c
@@ -1,5 +1,5 @@
 /* Find line information for address.
-   Copyright (C) 2004, 2005 Red Hat, Inc.
+   Copyright (C) 2004, 2005, 2014 Red Hat, Inc.
    This file is part of elfutils.
    Written by Ulrich Drepper <drepper@redhat.com>, 2004.
 
@@ -45,32 +45,28 @@ dwarf_getsrc_die (Dwarf_Die *cudie, Dwarf_Addr addr)
     return NULL;
 
   /* The lines are sorted by address, so we can use binary search.  */
-  size_t l = 0, u = nlines;
-  while (l < u)
-    {
-      size_t idx = (l + u) / 2;
-      if (addr < lines->info[idx].addr)
-	u = idx;
-      else if (addr > lines->info[idx].addr || lines->info[idx].end_sequence)
-	l = idx + 1;
-      else
-	return &lines->info[idx];
-    }
-
   if (nlines > 0)
-    assert (lines->info[nlines - 1].end_sequence);
-
-  /* If none were equal, the closest one below is what we want.  We
-     never want the last one, because it's the end-sequence marker
-     with an address at the high bound of the CU's code.  If the debug
-     information is faulty and no end-sequence marker is present, we
-     still ignore it.  */
-  if (u > 0 && u < nlines && addr > lines->info[u - 1].addr)
     {
-      while (lines->info[u - 1].end_sequence && u > 0)
-	--u;
-      if (u > 0)
-	return &lines->info[u - 1];
+      size_t l = 0, u = nlines - 1;
+      while (l < u)
+	{
+	  size_t idx = u - (u - l) / 2;
+	  Dwarf_Line *line = &lines->info[idx];
+	  if (addr < line->addr)
+	    u = idx - 1;
+	  else
+	    l = idx;
+	}
+
+      /* This is guaranteed for us by libdw read_srclines.  */
+      assert (lines->info[nlines - 1].end_sequence);
+
+      /* The last line which is less than or equal to addr is what we
+	 want, unless it is the end_sequence which is after the
+	 current line sequence.  */
+      Dwarf_Line *line = &lines->info[l];
+      if (! line->end_sequence && line->addr <= addr)
+	return &lines->info[l];
     }
 
   __libdw_seterrno (DWARF_E_ADDR_OUTOFRANGE);

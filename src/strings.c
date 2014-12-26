555fdae16cea49d0540858b3acb3d2f3e63c123c
Mark Wielaard <mjw@redhat.com>
2014-12-26 22:57:00 +0100

    strings: Produce error when section data falls outside file.
    https://bugzilla.redhat.com/show_bug.cgi?id=1170810
    
    Reported-by: Alexander Cherepanov <cherepan@mccme.ru>
    Signed-off-by: Mark Wielaard <mjw@redhat.com>

--- src/strings.c
+++ src/strings.c
@@ -725,8 +725,21 @@ read_elf (Elf *elf, int fd, const char *fname, off64_t fdlen)
 	 actually have content.  */
       if (shdr != NULL && shdr->sh_type != SHT_NOBITS
 	  && (shdr->sh_flags & SHF_ALLOC) != 0)
-	result |= read_block (fd, fname, fdlen, shdr->sh_offset,
-			      shdr->sh_offset + shdr->sh_size);
+	{
+	  if (shdr->sh_offset > (Elf64_Off) fdlen
+	      || fdlen - shdr->sh_offset < shdr->sh_size)
+	    {
+	      size_t strndx = 0;
+	      elf_getshdrstrndx (elf, &strndx);
+	      error (0, 0,
+		     gettext ("Skipping section %zd '%s' data outside file"),
+		     elf_ndxscn (scn), elf_strptr (elf, strndx, shdr->sh_name));
+	      result = 1;
+	    }
+	  else
+	    result |= read_block (fd, fname, fdlen, shdr->sh_offset,
+				  shdr->sh_offset + shdr->sh_size);
+	}
     }
   while ((scn = elf_nextscn (elf, scn)) != NULL);
 

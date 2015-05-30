fc9b79400a7bf1aa399cadf5e0351d757a4e44a6
Mark Wielaard <mjw@redhat.com>
2015-05-30 23:50:14 +0200

    libelf: Don't call memmove with possible NULL buffer.
    When size is zero the buffer src and dest buffers might be NULL.
    
    Signed-off-by: Mark Wielaard <mjw@redhat.com>

--- libelf/gelf_xlate.c
+++ libelf/gelf_xlate.c
@@ -1,5 +1,5 @@
 /* Transformation functions for ELF data types.
-   Copyright (C) 1998,1999,2000,2002,2004,2005,2006,2007 Red Hat, Inc.
+   Copyright (C) 1998,1999,2000,2002,2004,2005,2006,2007,2015 Red Hat, Inc.
    This file is part of elfutils.
    Written by Ulrich Drepper <drepper@redhat.com>, 1998.
 
@@ -52,7 +52,8 @@ static void
 (elf_cvt_Byte) (void *dest, const void *src, size_t n,
 		int encode __attribute__ ((unused)))
 {
-  memmove (dest, src, n);
+  if (n != 0)
+    memmove (dest, src, n);
 }
 
 

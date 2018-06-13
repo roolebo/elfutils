6ad1343200116145aae9e3f04f4973a7aaf3a999
Andreas Schwab <schwab@suse.de>
2018-06-13 12:28:00 +0200

    backends: add abi_cfi and register_info callbacks for RISC-V
    From
    https://github.com/riscv/riscv-isa-manual/raw/master/release/riscv-spec-v2.2.pdf
    and GCC source.
    
    Signed-off-by: Andreas Schwab <schwab@suse.de>

--- backends/riscv_init.c
+++ backends/riscv_init.c
@@ -51,6 +51,10 @@ riscv_init (Elf *elf __attribute__ ((unused)),
   eh->name = "RISC-V";
   riscv_init_reloc (eh);
   HOOK (eh, reloc_simple_type);
+  HOOK (eh, register_info);
+  HOOK (eh, abi_cfi);
+  /* gcc/config/ #define DWARF_FRAME_REGISTERS.  */
+  eh->frame_nregs = 66;
   HOOK (eh, check_special_symbol);
   HOOK (eh, machine_flag_check);
 
